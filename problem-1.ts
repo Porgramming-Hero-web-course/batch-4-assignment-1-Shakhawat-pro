029d929a3c54d4424ab815f046f21768a8de025a
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-11-07 01:09:08 +0600

    problem 1 done

--- problem-1.ts
+++ problem-1.ts
@@ -1,1 +1,9 @@
-console.log("hello world");
+const number = [1, 8, 6, 8, 1, 7, 3]
+const number2 = [1, 2, 3, 4, 5, 6]
+
+function sumArray(numbers: number[]): number {
+    return numbers.reduce((sum, current) => sum + current, 0)   
+}
+
+console.log(sumArray(number));     //output - 34
+console.log(sumArray(number2));    //output - 21
